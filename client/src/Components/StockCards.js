ee914bcd8a7c8e66afaebb7d5176e61a5806a789
Mario Granados <mario.alberto.granados1@gmail.com>
2023-07-18 19:32:15 -0500

    fixed some bugs

--- client/src/Components/StockCards.js
+++ client/src/Components/StockCards.js
@@ -1,20 +1,37 @@
-import React from 'react'
-import Card from 'react-bootstrap/Card'
-import Button from 'react-bootstrap/Button'
+import React from "react";
+import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
+import Placeholder from 'react-bootstrap/Placeholder'
 
-export default function Cards({name, price}) {
+export default function Cards(props) {
+  const { name, price } = props.props;
+  if (!props) {
+    return (
+      <Card style={{width: '18rem'}}>
+        <Card.Body>
+          <Placeholder as={Card.Title} animation="glow">
+            <Placeholder xs={6} />
+          </Placeholder>
+          <Placeholder as={Card.Text} animation="glow">
+            <Placeholder xs={7} /> <Placeholder xs={4} /> <Placeholder xs={4} />{" "}
+            <Placeholder xs={6} /> <Placeholder xs={8} />
+          </Placeholder>
+          <Placeholder.Button variant="primary" xs={6} />
+        </Card.Body>
+      </Card>
+    );
+  }
   return (
     <>
-    <Card>
+      <Card style={{width: '18rem'}}>
         <Card.Body>
-            <Card.Title>{name}</Card.Title>
-            <Card.Subtitle>{price}</Card.Subtitle>
-            <Card.Text> lorem ipsum for es late! </Card.Text>
-            <Card.Link>Chart</Card.Link>
-            <Button variant='delete'>Remove from portfolio</Button>
+          <Card.Title>{name}</Card.Title>
+          <Card.Subtitle>{price}</Card.Subtitle>
+          <Card.Text> lorem ipsum for es late! </Card.Text>
+          <Card.Link>Chart</Card.Link>
+          <Button variant="delete">Remove from portfolio</Button>
         </Card.Body>
-    </Card>
-        
+      </Card>
     </>
-  )
+  );
 }
--- client/src/Pages/HomePage.js
+++ client/src/Pages/HomePage.js
@@ -1,12 +1,9 @@
 import React, { useEffect, useState } from "react";
 import { API_URL } from "../Config/Confg";
-import StockCards from "../Components/StockCards";
 import Cards from "../Components/StockCards";
-import Spinner from 'react-bootstrap/Spinner'
-
+import Spinner from "react-bootstrap/Spinner";
 export default function HomePage() {
   const [dataDocument, setDataDocument] = useState([]);
-  let append = [];
   async function getRequest() {
     const response = await fetch(`${API_URL}/all`);
     const data = await response.json();
@@ -16,13 +13,15 @@ export default function HomePage() {
     getRequest();
   }, []);
 
-  if (dataDocument.length != 0) {
-    return <>
-    {dataDocument.map((item, index)=>{
-         return <Cards props={item} key={index}/>
-     })}
-    </>;
+  if (dataDocument.length !== 0) {
+    return (
+      <>
+        {dataDocument.map((item, index) => {
+          return <Cards props={item} key={index} />;
+        })}
+      </>
+    );
   } else {
-    return <><Spinner animation="grow" /></>;
+    return <Spinner animation="grow" variant="primary" />;
   }
 }
