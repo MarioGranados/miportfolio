97edecdc7681baddcfabcc42bcae4b07993e4282
Mario Granados <mario.alberto.granados1@gmail.com>
2023-07-19 11:29:16 -0500

    testing json

--- client/src/Pages/Stock.js
+++ client/src/Pages/Stock.js
@@ -1,65 +1,76 @@
 import React, { useState } from "react";
 import { useEffect } from "react";
 import { STOCK_API_KEY } from "../Config/Confg";
-import { Bar } from 'react-chartjs-2';
+import { Chart } from "react-google-charts";
 
 export default function Stock() {
   const [symbol, setSymbol] = useState("IBM");
-  const URL = `https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=${symbol}&interval=5min&apikey=${STOCK_API_KEY}`;
-  console.log(symbol);
+  const [stockData, setStockData] = useState({});
+  const [day, setDay] = useState([]);
+  const [info, setInfo] = useState([]);
+  //const URL = `https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=${symbol}&interval=60min&apikey=${STOCK_API_KEY}`;
+  const URL = './client/src/Pages/data.json'
 
   async function fetchStockData() {
-    const response = await fetch(`${URL}`);
+    const response = await fetch(`${URL}`, {
+      headers : { 
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+       }
+    });
     const data = await response.json();
-    console.log(data);
+    setStockData(data)
+    console.log(data)
   }
 
-// utils/Data.js
-const d = [
-  {
-    id: 1,
-    year: 2016,
-    userGain: 80000,
-    userLost: 823
-  },
-  {
-    id: 2,
-    year: 2017,
-    userGain: 45677,
-    userLost: 345
-  },
-  {
-    id: 3,
-    year: 2018,
-    userGain: 78888,
-    userLost: 555
-  },
-  {
-    id: 4,
-    year: 2019,
-    userGain: 90000,
-    userLost: 4555
-  },
-  {
-    id: 5,
-    year: 2020,
-    userGain: 4300,
-    userLost: 234
-  }
-];
+  // if(stockData != null) {
+  // const day = Object.keys(stockData["Time Series (60min)"]);
+  // const results = Object.values(stockData["Time Series (60min)"]);
+  // }
+
+  //Convert Day and results to data format shown below
+  // day open close low high
+  //  const chartdata = [
+  //   ["day", "a", "b", "c", "d"],
+  //   ["Mon", 20, 28, 38, 45],
+  //   ["Tue", 31, 38, 55, 66],
+  //   ["Wed", 50, 55, 77, 80],
+  //   ["Thu", 50, 77, 66, 77],
+  //   ["Fri", 15, 66, 22, 68],
+  // ];
+  // const chartInputData = [];
+  // let arr = [];
+  // const chartInfo = ["Day", "Open Close", "High", "Low", "Close"];
+  // chartInputData.push(chartInfo)
+
+  // for(let i = 0; i < day.length; i++) {
+  //   arr.push(day[i]);
+  //   arr.push(Number(results[i]["1. open"]))
+  //   arr.push(Number(results[i]["2. high"]));
+  //   arr.push(Number(results[i]["3. low"]));
+  //   arr.push(Number(results[i]["4. close"]));
+  //   chartInputData.push(arr);
+  //   arr = [];
+  // }
+
+  // console.log('chart input')
+  // console.log(chartInputData)
+
+  const options = {
+    legend: "none",
+  };
 
   useEffect(() => {
     fetchStockData();
-  }, [symbol]);
+  }, []);
 
   return (
-    <Bar
-      data={data}
-      height={400}
-      width={600}
-      options={{
-        maintainAspectRatio: false,
-      }}
+    <Chart
+      chartType="CandlestickChart"
+      width="100%"
+      height="400px"
+      // data={data}
+      options={options}
     />
   );
 }
